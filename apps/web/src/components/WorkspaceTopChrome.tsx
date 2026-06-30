d9f335191b6da86f354555b2cdffff0a1163e3d9
taehallm <uv.taeha@gmail.com>
2026-06-30 17:02:20 +0900

    refactor(workspace): extract top chrome surface

---
+++ apps/web/src/components/WorkspaceTopChrome.tsx
@@ -0,0 +1,149 @@
+import type { ComponentProps } from "react";
+import { FileTabs } from "./FileTabs";
+import { ShareControls } from "./ShareControls";
+import { TopChrome } from "./TopChrome";
+import type { Collaborator } from "../collab";
+import type { JsonShareController } from "../hooks/useJsonShareController";
+import type { WorkspaceLanguage } from "../hooks/useWorkspacePreferences";
+import type { SharePanel } from "../uiTypes";
+import type { WorkspaceFile } from "../workspaceStorage";
+
+type FileTabsProps = ComponentProps<typeof FileTabs>;
+
+type WorkspaceTopChromeProps = {
+  activeFile?: WorkspaceFile;
+  activeFileTitle: string;
+  activeText: string;
+  canStartSession: boolean;
+  collaborators: Collaborator[];
+  copied: boolean;
+  currentUserName: string;
+  files: WorkspaceFile[];
+  getFileStatus: FileTabsProps["getFileStatus"];
+  identity: Collaborator;
+  isLive: boolean;
+  jsonShare: JsonShareController;
+  language: WorkspaceLanguage;
+  openFiles: WorkspaceFile[];
+  rightPanelOpen: boolean;
+  shareOpen: boolean;
+  sharePanelTarget?: SharePanel;
+  startSessionUnavailableReason: string;
+  workspaceMenuOpen: boolean;
+  onAddFile: FileTabsProps["onAddFile"];
+  onChangeUserName: (nextName: string) => void;
+  onChromeInteraction: NonNullable<FileTabsProps["onChromeInteraction"]>;
+  onCloseFile: FileTabsProps["onCloseFile"];
+  onCloseShare: () => void;
+  onCommitUserName: () => void;
+  onCopyFile: () => void;
+  onCopyShareUrl: () => void;
+  onDownloadFile: () => void;
+  onDownloadProjectArchive: () => void;
+  onReorderFiles: FileTabsProps["onReorderFiles"];
+  onRenameFile: FileTabsProps["onRenameFile"];
+  onSelectFile: FileTabsProps["onSelectFile"];
+  onStartSession: () => void;
+  onStopSession: () => void;
+  onToggleRightPanel: () => void;
+  onToggleShare: () => void;
+  onToggleWorkspaceMenu: () => void;
+};
+
+export function WorkspaceTopChrome({
+  activeFile,
+  activeFileTitle,
+  activeText,
+  canStartSession,
+  collaborators,
+  copied,
+  currentUserName,
+  files,
+  getFileStatus,
+  identity,
+  isLive,
+  jsonShare,
+  language,
+  openFiles,
+  rightPanelOpen,
+  shareOpen,
+  sharePanelTarget,
+  startSessionUnavailableReason,
+  workspaceMenuOpen,
+  onAddFile,
+  onChangeUserName,
+  onChromeInteraction,
+  onCloseFile,
+  onCloseShare,
+  onCommitUserName,
+  onCopyFile,
+  onCopyShareUrl,
+  onDownloadFile,
+  onDownloadProjectArchive,
+  onReorderFiles,
+  onRenameFile,
+  onSelectFile,
+  onStartSession,
+  onStopSession,
+  onToggleRightPanel,
+  onToggleShare,
+  onToggleWorkspaceMenu,
+}: WorkspaceTopChromeProps) {
+  const fileTabs = (
+    <FileTabs
+      files={openFiles}
+      activeFile={activeFile}
+      activeCollaboratorCount={collaborators.length}
+      getFileStatus={getFileStatus}
+      onAddFile={onAddFile}
+      onSelectFile={onSelectFile}
+      onRenameFile={onRenameFile}
+      onCloseFile={onCloseFile}
+      onReorderFiles={onReorderFiles}
+      onChromeInteraction={onChromeInteraction}
+    />
+  );
+
+  const shareControls = activeFile ? (
+    <ShareControls
+      activeFile={activeFile}
+      files={files}
+      activeFileTitle={activeFileTitle}
+      language={language}
+      currentUserName={currentUserName}
+      canStartSession={canStartSession}
+      isLive={isLive}
+      shareOpen={shareOpen}
+      sharePanelTarget={sharePanelTarget}
+      copied={copied}
+      jsonShare={jsonShare}
+      startSessionUnavailableReason={startSessionUnavailableReason}
+      onToggleShare={onToggleShare}
+      onCloseShare={onCloseShare}
+      onStartSession={onStartSession}
+      onCopyShareUrl={onCopyShareUrl}
+      onCopyFile={onCopyFile}
+      onDownloadFile={onDownloadFile}
+      onDownloadProjectArchive={onDownloadProjectArchive}
+      onChangeUserName={onChangeUserName}
+      onCommitUserName={onCommitUserName}
+      onStopSession={onStopSession}
+    />
+  ) : null;
+
+  return (
+    <TopChrome
+      workspaceMenuOpen={workspaceMenuOpen}
+      rightPanelOpen={rightPanelOpen}
+      isLive={isLive}
+      language={language}
+      identity={identity}
+      collaborators={collaborators}
+      activeText={activeText}
+      fileTabs={fileTabs}
+      shareControls={shareControls}
+      onToggleWorkspaceMenu={onToggleWorkspaceMenu}
+      onToggleRightPanel={onToggleRightPanel}
+    />
+  );
+}
